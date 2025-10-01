21e25021e8880ac594a6736a3ca448386fd52ff2
Enrico <e.giacomazzi@contralto-audio.com>
2025-10-01 09:53:05 +0200

    form completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import {clsx} from 'clsx';
 import style from './App.module.css';
 
 
+const validators = {
+    firstname: value => value.length > 0,
+    lastname: value => value.length > 3,
+    username: value => value.length > 0 && value.startsWith('@')
+}
+
 
 const App = () => {
   const [formValue, setFormValue] = useState({
@@ -29,28 +35,30 @@ const App = () => {
     console.log(formValue);
   }
 
-  const inputChangeHandler = (e) => {
-    const val = e.target.value.replace(/\D/g, '');
-    setText(val);
-    // console.log('change', val);
-  }
-
-
   function formHandler(e) {
     const value = e.target.value;
     const field = e.target.name;
     setFormValue(f => ({...f, [field]: value}));
+    setFormTouched(f => ({...f, [field]: true}));
+
+    const validator = validators[field];
+    setFormValid(f => ({...f, [field]: validator(value)}));
   }
 
+  const inputCls = (field) => {
+    return (!formValid[field] && formTouched[field]) ? 'invalid' : '';
+  }
 
   return (
     <>
       <form onSubmit={submitHandler}>
-        <input name="firstname" className={(!formValid.firstname && formTouched.firstname) ? 'invalid' : ''} type="text" value={formValue.firstname} onChange={formHandler}></input>
-        <input name="lastname" type="text" value={formValue.lastname} onChange={formHandler}></input>
-        <input name="username" type="text" value={formValue.username} onChange={formHandler}></input>
+        <input name="firstname" className={inputCls('firstname')} type="text" value={formValue.firstname} onChange={formHandler}></input>
+        <input name="lastname" className={inputCls('lastname')} type="text" value={formValue.lastname} onChange={formHandler}></input>
+        <input name="username" className={inputCls('username')} type="text" value={formValue.username} onChange={formHandler}></input>
         <br />
         <input type="submit" value="Send" />
+        <br/>
+        <pre>{JSON.stringify(formTouched)}</pre>
       </form>
     </>
 
