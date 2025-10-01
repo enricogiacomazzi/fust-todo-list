406debe32b416a7ad788f532b687886b9099e210
Enrico <e.giacomazzi@contralto-audio.com>
2025-10-01 12:12:48 +0200

    formik 101

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,75 @@ import './App.css';
 import {useState, useRef} from 'react';
 import {clsx} from 'clsx';
 import style from './App.module.css';
-import { useForm } from "react-hook-form"
+import { Formik } from 'formik';
 import { MyInput } from './components/MyInput';
 
 
 const App = () => {
-  const frm = useForm();
-  const {register, watch, handleSubmit, formState: {errors, dirtyFields}} = frm;
 
-  const inputCls = (field) => {
-    console.log(errors[field]);
-    return !!errors[field] ? 'invalid' : '';
+  const onSub = (values) => {
+    console.log('sub', values);
   }
-
-  const onSubmit = (data) => {
-    console.log('ciao', data);
-  }
-
-  console.log(watch('firstname'));
 
   return (
-    <>
-      <form className="row g-3" onSubmit={handleSubmit(onSubmit)}>
-        <MyInput label="Nome" inputName="firstname" form={frm} registerProps={{required: true}} />
-        <MyInput label="Cognome" inputName="lastname" form={frm} registerProps={{required: true, minLength: 5}} />
-        <MyInput label="Username" inputName="username" form={frm} registerProps={{required: true, pattern: /^@\w+$/}}/>
-        
-        <br />
-        <input className="btn btn-primary" type="submit" value="Send" />
-        <br/>
-      </form>
-    </>
+    <Formik 
+      initialValues={{firstname: 'Mario', lastname: ''}}
+      onSubmit={onSub}
+      validate={values => {
+        const errors = {};
 
+        if(!values.firstname || values.firstname?.length < 3) {
+          errors.firstname = 'firstname required';
+        }
+
+        if(!values.lastname) {
+          errors.lastname = 'lastname required';
+        }
+
+        return errors;
+
+      }}
+
+    >
+      {({values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,}) => (
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              name="firstname"
+              className={clsx([
+              'form-control', 
+              touched.firstname && errors.firstname && 'is-invalid',
+              touched.firstname && !errors.firstname && 'is-valid'
+              ])} 
+              onChange={handleChange} 
+              onBlur={handleBlur} 
+              value={values.firstname}
+            ></input>
+            <input 
+              type="text" 
+              name="lastname" 
+              className={clsx([
+              'form-control', 
+              touched.lastname && errors.lastname && 'is-invalid',
+              touched.lastname && !errors.lastname && 'is-valid'
+              ])} 
+              onChange={handleChange} 
+              onBlur={handleBlur} 
+              value={values.lastname}
+            ></input>
+            <br/>
+            <input type="submit" value="invia"></input>
+            <br/>
+            <pre>{errors.firstname}</pre>
+          </form>
+      )}
+    </Formik>
   )
 }
 
